7660b5058e17bf9a798123ab82ee20d6d021e127
albertanhduy <albertanhduy@gmail.com>
2017-10-16 20:22:15 +0700

    Extract

---
+++ OnlineShop16-10-2017/OnlineShop16-10-2017/TheOnlineShop/Model/Dao/UserDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public User GetByID(string UserName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == UserName);
+        }
+
+        public bool Login(string Username, string Password)
+        {
+            var result = db.Users.Count(x => x.UserName == Username && x.Password == Password);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OnlineShop16-10-2017/OnlineShop16-10-2017/TheOnlineShop/TheOnlineShop/Areas/Admin/Controllers/SignInController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheOnlineShop.Areas.Admin.Models;
+using Model.Dao;
+using Model.EF;
+using TheOnlineShop.Common;
+
+namespace TheOnlineShop.Areas.Admin.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: Admin/SignIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(SignInModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.UserName, model.Password);
+                if (result)
+                {
+                    var user = dao.GetByID(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không đúng");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
